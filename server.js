4324ea15be6812265481cd8a593b9a03671ed479
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-12-30 17:55:20 +0000

    changes

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const serveStatic = require("serve-static")
-const path = require('path')
+/**************************************************************************
+IMPORTS
+**************************************************************************/
 
-const app = express()
 
-app.use("/", serveStatic(path.join(__dirname, '/dist')))
+var express = require('express')
+var history = require('connect-history-api-fallback')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const port = process.env.PORT || 8080
 
-app.listen(port)
+/**************************************************************************
+EXPRESS SERVER
+**************************************************************************/
 
-console.log("Listening on port: " + port)
+
+var app = express()
+
+app.use(history({verbose: true}))
+app.use(serveStatic(path.join(__dirname, '/dist')))
+
+var port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log('Server started at http://localhost:5000')
+})
