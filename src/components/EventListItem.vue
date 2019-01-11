ea2ac9ee6a07aaca3f223228970a5d5bebce69fd
Gustavo <gustavofamoso48@gmail.com>
2019-01-11 22:36:44 +0000

    changes

--- src/components/EventListItem.vue
+++ src/components/EventListItem.vue
@@ -7,22 +7,27 @@
 			<router-link :to="to" class="router-link">
 				<h5 class="text-white">{{ event.name }}</h5>
 			</router-link>
-			<div class="text-atlas3 event-card-body d-flex">
-				<div class="col-lg-3">
-					<i class="fa fa-map-marker-alt text-atlas1" aria-hidden="true"></i>
-					{{ event.classroom }}
+			<div class="text-atlas3 event-card-body d-flex align-content-end flex-wrap">
+				<div class="col-12 col-sm-4">
+					<template v-for="eventTag in event.tags">{{ "#" + getTagById(eventTag).name + " " }}</template>
 				</div>
-				<div class="col-lg-3">
-					<i class="fa fa-calendar-alt text-atlas1" aria-hidden="true"></i>
-					daqui a 3 dias
-				</div>
-				<div class="col-lg-3">
-					<i class="fa fa-microphone-alt text-atlas1" aria-hidden="true"></i>
-					{{ getUserById(event.authorId).name }}
-				</div>
-				<div class="col-lg-3">
-					<i class="fa fa-users text-atlas1" aria-hidden="true"></i>
-					21 foram
+				<div class="col-12 col-sm-8 row pull-right">
+					<div class="mr-2">
+						<i class="fa fa-map-marker-alt text-atlas1" aria-hidden="true"></i>
+						{{ event.classroom }}
+					</div>
+					<div class="mr-2">
+						<i class="fa fa-calendar-alt text-atlas1" aria-hidden="true"></i>
+						daqui a 3 dias
+					</div>
+					<div class="mr-2">
+						<i class="fa fa-microphone-alt text-atlas1" aria-hidden="true"></i>
+						{{ getUserById(event.authorId).name }}
+					</div>
+					<div class="mr-2">
+						<i class="fa fa-users text-atlas1" aria-hidden="true"></i>
+						53 foram
+					</div>
 				</div>
 			</div>
 		</div>
@@ -35,7 +40,7 @@ import { mapGetters } from "vuex"
 export default {
 	props: ["to", "event"],
 	computed: {
-		...mapGetters(["getUserById"])
+		...mapGetters(["getUserById", "getTagById"])
 	}
 }
 </script>
