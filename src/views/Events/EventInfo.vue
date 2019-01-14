ed40fd8381394e4d65fa2201934c2a00a56241e8
Gustavo <gustavofamoso48@gmail.com>
2019-01-14 21:04:39 +0000

    changes

--- src/views/Events/EventInfo.vue
+++ src/views/Events/EventInfo.vue
@@ -1,7 +1,38 @@
 <template>
 	<div>
-        <TitleAtlas>{{  }}</TitleAtlas>
-    </div>
+		<TitleAtlas>{{ event.name }}</TitleAtlas>
+		<div class="row">
+			<div class="col-xl-4 col-lg-3 col-md-5 col-6">
+				<img :src="event.picture.poster" class="img-fluid img-thumbnail">
+			</div>
+			<div class="col-xl-8 col-lg-9 col-md-5 col-12">
+				<div>
+					<h4>Detalhes:</h4>
+					{{ event.description }}
+				</div>
+				<div class="mt-4">
+					<template v-for="tag in event.tags">#{{ getTagById(tag).name }}&nbsp;</template>
+				</div>
+
+				<hr class="bg-atlas1">
+				<div class="row">
+					<button
+						class="col-lg-4 col-md-9 col-sm-20 btn btn-atlas2 px-1 mr-auto ml-auto"
+						style="border-radius: 2em;"
+					>
+						<i class="fa fa-star"></i> Com interesse
+					</button>
+					<button
+						class="col-lg-4 col-md-9 col-sm-20 btn btn-atlas2 px-2 mr-auto ml-auto"
+						style="border-radius: 2em;"
+					>
+						<i class="fa fa-archive text-teca4"></i> Inscrever-me
+					</button>
+				</div>
+				<hr class="bg-atlas1">
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -10,17 +41,17 @@ import { mapGetters } from "vuex"
 
 export default {
 	name: "EventInfoView",
-    components: { TitleAtlas },
-    created() {
-        this.event = this.getEventById(this.$route)
-    },
-    data() {
-        return {
-            event: {}
-        }
-    },
-    computed: {
-        ...mapGetters(["getEventById"])
-    }
+	components: { TitleAtlas },
+	created() {
+		this.event = this.getEventById(parseInt(this.$route.params.id))
+	},
+	data() {
+		return {
+			event: {}
+		}
+	},
+	computed: {
+		...mapGetters(["getEventById", "getTagById"])
+	}
 }
 </script>
