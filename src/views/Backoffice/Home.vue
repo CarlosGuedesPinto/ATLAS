943cbefec3849d3b4bb1abdfb603afdb14cacda7
Gustavo <gustavofamoso48@gmail.com>
2019-01-24 04:21:41 +0000

    changes

--- src/views/Backoffice/Home.vue
+++ src/views/Backoffice/Home.vue
@@ -1,42 +1,27 @@
 <template>
-    <div>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-    </div>
+	<div>
+		<h4>Bem vindo ao Painel de Controlo do ATLAS!</h4>
+		<p>Permissões:</p>
+		<ul v-if="getUserById(getLoggedUserId).profileId === 3">
+			<li>Adicionar utilizadores. Ver todos os utilizadores.</li>
+			<li>Adicionar/remover tags. Ver todas as tags.</li>
+			<li>Adicionar/remover cursos. Ver todos os cursos.</li>
+			<li>Adicionar evento. Ver todos os eventos.</li>
+		</ul>
+		<ul v-else>
+			<li>Adicionar evento. Ver todos os seus eventos.</li>
+		</ul>
+	</div>
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
-    name: "BackofficeHomeView"
+	name: "BackofficeHomeView",
+	computed: {
+		...mapGetters(["getUserById", "getLoggedUserId"])
+	}
 }
 </script>
 
