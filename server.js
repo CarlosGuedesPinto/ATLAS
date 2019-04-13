c1256e7c87778e53390de3b61cb2e1048a08420c
Carlos Guedes <9170138@esmad.ipp.pt>
2019-04-13 19:09:20 +0000

    Added history middleware

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express');
+const express = require("express")
 const serveStatic = require("serve-static")
-const path = require('path');
-const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8080;
-app.listen(port);
+const path = require("path")
+const history = require("connect-history-api-fallback")
+const app = express()
+
+app.use(history())
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+const port = process.env.PORT || 8080
+
+app.listen(port)
